2f54c55b67c2c4caafbf8968623ce457308e733e
Joseph Hamilton <117998447+JBrosDevelopment@users.noreply.github.com>
2023-07-07 19:01:27 -0500

    JL

--- Assets/scripts_JL/Main_Duck.cs
+++ Assets/scripts_JL/Main_Duck.cs
@@ -13,6 +13,7 @@ public class Main_Duck : MonoBehaviour
     public float _JumpSpeed = 10;
     public int MaxJumps = 2;
     public float JumpCooldown = 0.2f;
+    public float ThirdJumpDrag = 10;
     public float _delta = 10000;
     [Tooltip("Shows: \nDeltatime, Jump(true/false), JumpCount, Can Jump")]
     public bool ShowDebug = false;
@@ -25,6 +26,7 @@ public class Main_Duck : MonoBehaviour
     bool Jump;
     bool isJumping = false;
     float jumpTimer = 0f;
+    bool dragging = false;
 
     IEnumerator Wait(float seconds)
     {
@@ -63,12 +65,21 @@ public class Main_Duck : MonoBehaviour
         rb.rotation = 0;
         rb.velocity = new Vector2(XInput * speed * delta, rb.velocity.y);
 
+        if(XInput == 0 && dragging)
+        {
+            rb.drag = 0;
+        }
+        else if (XInput != 0 && dragging)
+        {
+            rb.drag = ThirdJumpDrag;
+        }
+
         if (isJumping && Time.time >= jumpTimer + JumpCooldown)
         {
             isJumping = false;
         }
 
-        if (Jump && !isJumping && (isGrounded || JumpCount < MaxJumps))
+        if (Jump && !isJumping)
         {
             rb.velocity = new Vector2(rb.velocity.x, 0);
             JumpCount++;
@@ -77,13 +88,24 @@ public class Main_Duck : MonoBehaviour
             jumpTimer = Time.time;
             rb.AddForce(transform.up * JumpSpeed * delta);
         }
+        else if(JumpCount >= MaxJumps && (Input.GetAxis("Jump") > 0 || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) && Time.time >= jumpTimer)
+        {
+            dragging = true;
+            isJumping = false;
+            Jump = false;
+            jumpTimer = Time.time;
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.drag = ThirdJumpDrag;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        dragging = false;
         JumpCount = 0;
         isGrounded = true;
         isJumping = false;
+        rb.drag = 0;
     }
 
     public void Death()
